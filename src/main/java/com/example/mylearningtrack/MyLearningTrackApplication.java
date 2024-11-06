3b7b1396276498d24c66dea72db6a46428abe72c
qiuqiu <qiujuanwang87@gmail.com>
2024-11-06 10:39:53 +0000

    Initial Commit

---
+++ src/main/java/com/example/mylearningtrack/MyLearningTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylearningtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLearningTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mylearningtrack/MyLearningTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylearningtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
