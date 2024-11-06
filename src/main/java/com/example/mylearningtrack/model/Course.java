4da58d32d217d3658792a44df9916ba63a78353c
qiuqiu <qiujuanwang87@gmail.com>
2024-11-06 12:01:40 +0000

    feat: add Course entity with code, name, credits, and relationships

---
+++ src/main/java/com/example/mylearningtrack/model/Course.java
@@ -0,0 +1,44 @@
+package com.example.mylearningtrack.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private int credits;
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<Module> modules;
+
+    @ManyToMany(mappedBy = "courses", fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<Student> students;
+
+}
