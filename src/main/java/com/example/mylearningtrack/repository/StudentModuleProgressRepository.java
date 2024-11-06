64f9b91b79e1c92854ee60854bfe95d74a5f03a9
qiuqiu <qiujuanwang87@gmail.com>
2024-11-06 14:02:51 +0000

    feat: add custom query to retrieve time spent by student for each module in StudentModuleProgressRepository

--- src/main/java/com/example/mylearningtrack/repository/StudentModuleProgressRepository.java
+++ src/main/java/com/example/mylearningtrack/repository/StudentModuleProgressRepository.java
@@ -2,8 +2,16 @@ package com.example.mylearningtrack.repository;
 
 import com.example.mylearningtrack.model.StudentModuleProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentModuleProgressRepository extends JpaRepository<StudentModuleProgress, Long> {
+
+    @Query("SELECT m.module.code, m.timeSpent FROM StudentModuleProgress m WHERE m.student.id = :studentId")
+    List<Object[]> findTimeSpentByStudent(@Param("studentId") Long studentId);
+
 }
