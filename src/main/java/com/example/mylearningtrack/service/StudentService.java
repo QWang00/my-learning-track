d23f43dc40b8cbd72c55e639aa7c84dacc974269
qiuqiu <qiujuanwang87@gmail.com>
2024-11-06 20:18:56 +0000

    feat: add method to retrieve result count by student in service interface

--- src/main/java/com/example/mylearningtrack/service/StudentService.java
+++ src/main/java/com/example/mylearningtrack/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.mylearningtrack.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface StudentService {
     List<Object[]> getTimeSpentByStudent(Long studentId);
+
+    Map<String,Double> getResultCountByStudent(Long studentId);
 }
