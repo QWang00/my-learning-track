fd25847e8f34c7bfec6bc85a24e34122788ea250
qiuqiu <qiujuanwang87@gmail.com>
2024-11-06 12:03:00 +0000

    feat: add StudentModuleProgress entity to track module progress, result, and time spent

---
+++ src/main/java/com/example/mylearningtrack/model/StudentModuleProgress.java
@@ -0,0 +1,36 @@
+package com.example.mylearningtrack.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentModuleProgress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "module_id")
+    private Module module;
+
+    @Column
+    private boolean completed;
+
+    @Column
+    private String result;
+
+    @Column
+    private int timeSpent;
+}
